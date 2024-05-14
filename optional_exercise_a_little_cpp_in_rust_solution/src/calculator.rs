f8c4061ef4997cc6c48badc9dc7341a475032405
Sidney <sidney.philipsen@alten.nl>
2024-05-14 19:59:01 +0200

    removed jansuun work

--- optional_exercise_a_little_cpp_in_rust_solution/src/calculator.rs
+++
@@ -1,6 +0,0 @@
-pub trait Calculator {
-    fn whothis(&self);
-    fn add(&self, x: i16, y: i16) -> i16;
-    fn subtract(&self, x: i32, y: i32) -> i32;
-    fn multiply(&self, x: u32, y: u32) -> u32;
-}
